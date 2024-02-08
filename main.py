221b8b1de7ee3d966c776116f5ad72b732d47dc8
asuoshig <144859297+asuoshig@users.noreply.github.com>
2024-02-08 18:54:35 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+#Leia 2 variáveis, denominadas A e B e faça a soma dessas duas variáveis, atribuindo seu resultado à variável X . Imprima X conforme mostrado abaixo. Imprima a linha final após o resultado, caso contrário você obterá “ Erro de apresentação ”.
+
+#Entrada
+#O arquivo de entrada conterá 2 números inteiros.
+
+#Saída
+#Imprima a letra X (maiúscula) com um espaço em branco antes e depois do sinal de igual seguido do valor de X, conforme exemplo a seguir.
+
+#Obs.: afinal não esqueça da linha final
+
+a= int(input())
+b=int(input())
+
+soma= a+b
+
+print(f'X = {soma}')
