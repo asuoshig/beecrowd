18a991893c730734109899e5a2eb05668b8fde8d
asuoshig <144859297+asuoshig@users.noreply.github.com>
2024-02-09 19:59:02 -0300

    Add files via upload

---
+++ exc3.py
@@ -0,0 +1,13 @@
+# A fórmula para calcular a área de uma circunferência é definida como A = π. R2 . _ Considerando para este problema que π = 3,14159 :
+
+# Calcule a área usando a fórmula fornecida na descrição do problema.
+
+# Entrada
+# A entrada contém um valor de ponto flutuante (dupla precisão), que é a variável R .
+
+# Saída
+# Apresente a mensagem "A=" seguida do valor da variável, como no exemplo abaixo, com quatro casas decimais após a vírgula. Use todas as variáveis ​​de precisão dupla. Como todos os problemas, não esqueça de imprimir o final da linha após o resultado, caso contrário receberá “Erro de apresentação”.
+
+raio= (float(input))
+area= ((raio ** 2) * 3.14159)
+print("A=%0.4f"%area)
